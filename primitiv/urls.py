161cc4f7e2dccccd9c3f7ceeb362c2f16407968b
SharlaiAA <artemsha06@gmail.com>
2025-06-02 09:17:31 +0300

    initial commit

--- primitiv/urls.py
+++ primitiv/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('view/', views.web1),
     path('', views.home, name = 'home'),
-    
+    path('login/', views.login_user, name = 'login'),
+    path('logout/', views.logout_user, name = 'logout'),
 ]
--- primitiv/views.py
+++ primitiv/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from . import templates
 
 
@@ -7,4 +9,25 @@ def web1(request):
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    if request.method == "POST":
+        user_name = request.POST['user_name']
+        password = request.POST['password']
+        #Authenticate
+        user = authenticate(request, username=user_name, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Вы успешно вошли в аккаунт")
+            return redirect('home')
+        else:
+            messages.success(request, 'Не получилось войти')
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+
+def login_user(request):
+    pass
+
+
+def logout_user(request):
+    pass
